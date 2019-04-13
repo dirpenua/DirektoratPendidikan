1c163e1ebe6f64348651c235756567c03661ac18
dirpenapp <dirpenua@gmail.com>
2019-04-13 10:07:09 +0700

    Initial Commit

--- app/src/main/java/com/example/direktoratpendidikan/DetailAgenda.java
+++ app/src/main/java/com/example/direktoratpendidikan/DetailAgenda.java
@@ -1,25 +1,41 @@
 package com.example.direktoratpendidikan;
 
+import android.app.Activity;
+import android.app.FragmentManager;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import com.example.direktoratpendidikan.data.Agenda;
 
 public class DetailAgenda extends AppCompatActivity {
 
     public String agenda_id;
+    public ImageView onback;
 
-
-    public boolean onSupportNavigateUp() {
-        onBackPressed();
-        return true;
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_agenda);
-
         this.agenda_id = getIntent().getStringExtra("idAgenda");
+        onback = (ImageView) findViewById(R.id.agendakembali);
+        onback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
+
 
     public String getId_agenda(){
         return this.agenda_id;
