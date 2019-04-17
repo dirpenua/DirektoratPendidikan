a4e74f77fc1bfdde9673e08810cf568c93d4e794
dirpenapp <dirpenua@gmail.com>
2019-04-18 02:43:19 +0700

    Initial Commit

--- app/src/main/java/com/example/direktoratpendidikan/UbahPWUserBaru.java
+++ app/src/main/java/com/example/direktoratpendidikan/UbahPWUserBaru.java
@@ -6,23 +6,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.example.direktoratpendidikan.R;
+public class UbahPWUserBaru extends AppCompatActivity {
 
-public class Login extends AppCompatActivity {
-
-    Button masuk;
+    Button ubah;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        masuk = (Button) findViewById(R.id.btn_login);
-        masuk.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_ubah_pwuser_baru);
+        ubah = (Button) findViewById(R.id.btn_ubah);
+        ubah.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(), UbahPWUserBaru.class);
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(i);
             }
         });
-
     }
 }
